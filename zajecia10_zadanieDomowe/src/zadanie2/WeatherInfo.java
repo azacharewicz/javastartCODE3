3bd122753675f7a88ad1faeea97a932394edc86d
Anna Zacharewicz <anna.zacharewicz@yahoo.com>
2017-11-10 12:51:27 +0100

    protected WeatherInfo

--- zajecia10_zadanieDomowe/src/zadanie2/WeatherInfo.java
+++ zajecia10_zadanieDomowe/src/zadanie2/WeatherInfo.java
@@ -5,7 +5,7 @@ public class WeatherInfo {
     private String description;
     private int temperature;
 
-    public WeatherInfo(String city, String description, int temperature) {
+    protected WeatherInfo(String city, String description, int temperature) {
         this.city = city;
         this.description = description;
         this.temperature = temperature;
