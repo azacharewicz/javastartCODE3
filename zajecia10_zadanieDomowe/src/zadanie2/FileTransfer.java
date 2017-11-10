037a19cf64a6eb436f55da44d3331e8354888cee
Anna Zacharewicz <anna.zacharewicz@yahoo.com>
2017-11-10 18:31:59 +0100

    powrót do punktu wyjścia...

--- zajecia10_zadanieDomowe/src/zadanie2/FileTransfer.java
+++ zajecia10_zadanieDomowe/src/zadanie2/FileTransfer.java
@@ -30,16 +30,22 @@ public class FileTransfer {
     public void pobierzWyswietlZapiszInformacjePogodowe() throws IOException {
         FileWriter fileWriter = new FileWriter("pogoda.csv", false); //dopisywanie BufferedWriter
         BufferedWriter bfw = new BufferedWriter(fileWriter);
-        //WeatherApi api = new WeatherApi();
+        WeatherApi api = new WeatherApi();
+
         System.out.println("\nINFORMACJE POGODOWE:");
         for (String city : listaMiast) {
             try {
-                pobierzInformacjePogodowe(api, city);
-                wyswietlInformacjePogodowe(weatherInfo);
-                //zapiszDoPlikuInformacjePogodowe(weatherInfo);
-                zapiszDoPlikuInformacjePogodowe(informacjePogodowe);
+                int temperature = api.getTemperature(city);
+                String description = api.getDescription(city);
+                WeatherInfo weatherInfo = new WeatherInfo(city, description, temperature);
+                informacjePogodowe.add(weatherInfo);
+                System.out.println(
+                        "Pogoda w mieście " + weatherInfo.getCity() + ": " + weatherInfo.getDescription() + ". "
+                                + "Aktualna temperatura " + weatherInfo.getTemperature() + " stopni");
+                //zapis do pliku pogoda.csv
+                fileWriter.write(weatherInfo.getCity() + ";" + weatherInfo.getTemperature() + ";" + weatherInfo.getDescription() + "\n");
             } catch (IOException e) {
-                System.err.println("Nie udało się pobrać informacji dla miasta " + weatherInfo.getCity());
+                System.err.println("Nie udało się pobrać informacji dla miasta " + city);
             }
         }
         fileWriter.close(); //ważne
@@ -47,56 +53,56 @@ public class FileTransfer {
     }
 
 
-    public void pobierzInformacjePogodowe(WeatherApi api, String city) throws IOException {
-        int temperature = api.getTemperature(city);
-        String description = api.getDescription(city);
-        weatherInfo = new WeatherInfo(city, description, temperature);
-        informacjePogodowe.add(weatherInfo);
-    }
 
-    public void wyswietlInformacjePogodowe(WeatherInfo weatherInfo) {
-        System.out.println(
-                "Pogoda w mieście " + weatherInfo.getCity() + ": " + weatherInfo.getDescription() + ". "
-                        + "Aktualna temperatura " + weatherInfo.getTemperature() + " stopni");
-    }
-
-
+//
+//    public void pobierzWyswietlZapiszInformacjePogodowe() throws IOException {
+//        FileWriter fileWriter = new FileWriter("pogoda.csv", false); //dopisywanie BufferedWriter
+//        BufferedWriter bfw = new BufferedWriter(fileWriter);
+//        //WeatherApi api = new WeatherApi();
+//        System.out.println("\nINFORMACJE POGODOWE:");
+//        for (String city : listaMiast) {
+//            try {
+//                pobierzInformacjePogodowe(api, city);
+//                wyswietlInformacjePogodowe(weatherInfo);
+//                zapiszDoPlikuInformacjePogodowe(weatherInfo);
+//                //zapiszDoPlikuInformacjePogodowe(informacjePogodowe); //ta medota zapisuje NULL w pliku csv
+//            } catch (IOException e) {
+//                System.err.println("Nie udało się pobrać informacji dla miasta " + weatherInfo.getCity());
+//            }
+//        }
+//        fileWriter.close(); //ważne
+//        System.out.println("\nZAPISANO DO PLIKU " + "pogoda.csv");
+//    }
+//
+//
+//    public void pobierzInformacjePogodowe(WeatherApi api, String city) throws IOException {
+//        int temperature = api.getTemperature(city);
+//        String description = api.getDescription(city);
+//        weatherInfo = new WeatherInfo(city, description, temperature);
+//        informacjePogodowe.add(weatherInfo);
+//    }
+//
+//    public void wyswietlInformacjePogodowe(WeatherInfo weatherInfo) {
+//        System.out.println(
+//                "Pogoda w mieście " + weatherInfo.getCity() + ": " + weatherInfo.getDescription() + ". "
+//                        + "Aktualna temperatura " + weatherInfo.getTemperature() + " stopni");
+//    }
+//
+//    //ta metoda zapisuje NULL w pliku csv
 //    public void zapiszDoPlikuInformacjePogodowe(WeatherInfo weatherInfo) throws IOException {
 //        FileWriter fileWriter = new FileWriter("pogoda.csv", false); //dopisywanie BufferedWriter
 //        fileWriter.write(weatherInfo.getCity() + ";" + weatherInfo.getTemperature() + ";" + weatherInfo.getDescription() + "\n");
 //    }
 
 
-    public void zapiszDoPlikuInformacjePogodowe(List<WeatherInfo> informacjePogodowe) throws IOException {
-        FileWriter fileWriter = new FileWriter("pogoda.csv", false); //dopisywanie BufferedWriter
-        fileWriter.write(informacjePogodowe.toString() + "\n");
-    }
-
-
-//    public void pobierzWyswietlZapiszInformacjePogodowe() throws IOException {
+    //ta metoda zapisuje NULL w pliku csv
+//    public void zapiszDoPlikuInformacjePogodowe(List<WeatherInfo> informacjePogodowe) throws IOException {
 //        FileWriter fileWriter = new FileWriter("pogoda.csv", false); //dopisywanie BufferedWriter
-//        BufferedWriter bfw = new BufferedWriter(fileWriter);
-//        WeatherApi api = new WeatherApi();
-//
-//        System.out.println("\nINFORMACJE POGODOWE:");
-//        for (String city : listaMiast) {
-//            try {
-//                int temperature = api.getTemperature(city);
-//                String description = api.getDescription(city);
-//                WeatherInfo weatherInfo = new WeatherInfo(city, description, temperature);
-//                informacjePogodowe.add(weatherInfo);
-//                System.out.println(
-//                        "Pogoda w mieście " + weatherInfo.getCity() + ": " + weatherInfo.getDescription() + ". "
-//                                + "Aktualna temperatura " + weatherInfo.getTemperature() + " stopni");
-//                //zapis do pliku pogoda.csv
-//                fileWriter.write(weatherInfo.getCity() + ";" + weatherInfo.getTemperature() + ";" + weatherInfo.getDescription() + "\n");
-//            } catch (IOException e) {
-//                System.err.println("Nie udało się pobrać informacji dla miasta " + city);
-//            }
-//        }
-//        fileWriter.close(); //ważne
-//        System.out.println("\nZAPISANO DO PLIKU " + "pogoda.csv");
+//        fileWriter.write(informacjePogodowe.toString() + "\n");
 //    }
+
+
+
 
 
 }
--- zajecia10_zadanieDomowe/src/zadanie2/WeatherApp.java
+++ zajecia10_zadanieDomowe/src/zadanie2/WeatherApp.java
@@ -10,5 +10,6 @@ public class WeatherApp {
 
         fileTransfer.odczytajMiastaZPliku();
         fileTransfer.pobierzWyswietlZapiszInformacjePogodowe();
+
     }
 }
