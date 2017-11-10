d27766d8ff7da13f8bd65b564ee1086270c558ae
Anna Zacharewicz <anna.zacharewicz@yahoo.com>
2017-11-10 12:11:27 +0100

    sam kontruktor

--- zajecia10_zadanieDomowe/src/zadanie2/WeatherInfo.java
+++ zajecia10_zadanieDomowe/src/zadanie2/WeatherInfo.java
@@ -1,37 +1,13 @@
 package zadanie2;
 
 public class WeatherInfo {
-    String city;
-    String description;
-    int temperature;
+    private String city;
+    private String description;
+    private int temperature;
 
     public WeatherInfo(String city, String description, int temperature) {
         this.city = city;
         this.description = description;
-        this.temperature = temperature;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(int temperature) {
         this.temperature = temperature;
     }
 }
